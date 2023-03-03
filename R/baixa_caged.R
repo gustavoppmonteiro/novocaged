117d83328a79bf1fca0978d9285c47f559d469a0
Gustavo Monteiro <gustavoppmonteiro@gmail.com>
2023-03-03 19:53:21 -0300

    opção de download com curl

--- R/baixa_caged.R
+++ R/baixa_caged.R
@@ -5,20 +5,19 @@
 #' @param mes_final Argumento opcional. No caso de baixar varios meses, se refere ao mes mais recente. Se for baixar apenas um mes o preenchimento e opcional.
 #'
 #' @export
-
 baixa_caged <- function(ano,
                         mes_inicial,
                         mes_final=mes_inicial){
-
+      
       # Referência ao pipe
       `%>%` <- magrittr::`%>%`
-
+      
       # meses
       meses <- sprintf("%02d", c(mes_inicial:mes_final))
-
+      
       # Baixa os microdados
       for (i in seq_along(meses)) {
-
+            
             # exclusoes
             origem = paste0("ftp://ftp.mtps.gov.br/pdet/microdados/NOVO CAGED/",
                             ano,
@@ -32,10 +31,18 @@ baixa_caged <- function(ano,
             destino = paste0("CAGEDEXC",
                              ano,
                              meses[i],".7z")
-
-            utils::download.file(url = origem, destfile = destino, mode = "wb")
-
-
+            
+            result <- tryCatch(
+                  {
+                        curl::curl_download(url = origem, destfile = destino, mode = "wb")
+                  },
+                  error = function(e) {
+                        utils::download.file(url = origem, destfile = destino, mode = "wb")
+                        NULL
+                  }
+            )
+            
+            
             # fora do prazo
             origem = paste0("ftp://ftp.mtps.gov.br/pdet/microdados/NOVO CAGED/",
                             ano,
@@ -49,10 +56,18 @@ baixa_caged <- function(ano,
             destino = paste0("CAGEDFOR",
                              ano,
                              meses[i],".7z")
-
-            utils::download.file(url = origem, destfile = destino, mode = "wb")
-
-
+            
+            result <- tryCatch(
+                  {
+                        curl::curl_download(url = origem, destfile = destino, mode = "wb")
+                  },
+                  error = function(e) {
+                        utils::download.file(url = origem, destfile = destino, mode = "wb")
+                        NULL
+                  }
+            )
+            
+            
             # movimentacao
             origem = paste0("ftp://ftp.mtps.gov.br/pdet/microdados/NOVO CAGED/",
                             ano,
@@ -66,11 +81,19 @@ baixa_caged <- function(ano,
             destino = paste0("CAGEDMOV",
                              ano,
                              meses[i],".7z")
-
-            utils::download.file(url = origem, destfile = destino, mode = "wb")
-
+            
+            result <- tryCatch(
+                  {
+                        curl::curl_download(url = origem, destfile = destino, mode = "wb")
+                  },
+                  error = function(e) {
+                        utils::download.file(url = origem, destfile = destino, mode = "wb")
+                        NULL
+                  }
+            )
+            
       }
-
+      
 }
 
 #TODO: tem que fazer excecoes para janeiro a março de 2020. Ver "D:\Dados\Bases de dados\novo caged\atualizacao_2020.R".
