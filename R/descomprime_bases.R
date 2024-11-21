08c1b5ec1ec02ff7d1b46225f29f484704282246
Gustavo Monteiro <gustavoppmonteiro@gmail.com>
2024-11-21 09:29:21 -0300

    Lista arquivos que começam com "CAGED"

--- R/descomprime_bases.R
+++ R/descomprime_bases.R
@@ -10,29 +10,33 @@ descomprime_bases <- function(){
 
       # Referência ao pipe
       `%>%` <- magrittr::`%>%`
-
-      # Descompactando na PastaMes
-      if(dir.exists("C:/Program Files/7-Zip/")){
-
-            unlink("./PastaMes", recursive=TRUE)
-            Sys.sleep(2)
-
-            system('"C:/Program Files/7-Zip/7z" e -oPastaMes CAGED*')
-            Sys.sleep(2)
-
-      }else if(dir.exists("C:/Program Files (x86)/7-Zip/")){
-
-            unlink("./PastaMes", recursive=TRUE)
-            Sys.sleep(2)
-
-            system('"C:/Program Files (x86)/7-Zip/" e -oPastaMes CAGED*')
-            Sys.sleep(2)
-
-      }else{
-            stop("Erro: 7zip nao instalado?")
+      
+      # Lista arquivos que começam com "CAGED"
+      arquivos_caged <- list.files(pattern = "^CAGED", full.names = TRUE)
+      
+      # Verifica se existem arquivos correspondentes
+      if (length(arquivos_caged) == 0) {
+            stop("Erro: Nenhum arquivo que começa com 'CAGED' foi encontrado.")
       }
-
+      
+      # Verificar se o 7-Zip está instalado em uma das pastas padrão
+      if (dir.exists("C:/Program Files/7-Zip/")) {
+            unlink("./PastaMes", recursive = TRUE)
+            Sys.sleep(2)
+            for (arquivo in arquivos_caged) {
+                  system(paste0("\"C:/Program Files/7-Zip/7z\" e -oPastaMes ", shQuote(arquivo)))
+            }
+            Sys.sleep(2)
+      } else if (dir.exists("C:/Program Files (x86)/7-Zip/")) {
+            unlink("./PastaMes", recursive = TRUE)
+            Sys.sleep(2)
+            for (arquivo in arquivos_caged) {
+                  system(paste0("\"C:/Program Files (x86)/7-Zip/7z\" e -oPastaMes ", shQuote(arquivo)))
+            }
+            Sys.sleep(2)
+      } else {
+            stop("Erro: 7zip não instalado?")
+      }
 }
-
 
 
